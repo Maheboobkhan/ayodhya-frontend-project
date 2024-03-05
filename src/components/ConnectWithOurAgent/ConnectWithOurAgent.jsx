2566b82027e2ccfb83e01e5a55e9da200f85fe0f
Maheboobkhan <maheboobkhan18202@gmail.com>
2024-03-05 14:41:28 +0530

    same commit

--- src/components/ConnectWithOurAgent/ConnectWithOurAgent.jsx
+++ src/components/ConnectWithOurAgent/ConnectWithOurAgent.jsx
@@ -27,6 +27,7 @@ const ConnectWithOurAgent = () => {
           setEmail("");
           setPhone("");
           setMessage("");
+          alert("Thank you for contacting us. We will get back to you soon.");
         })
       };
 
